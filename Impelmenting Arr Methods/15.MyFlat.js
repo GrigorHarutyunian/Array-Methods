c6c57478f51b7f1ac81e1b95da3ef1d7209adcda
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-08-18 20:35:05 +0400

    version 2 for MyFlat

--- Impelmenting Arr Methods/15.MyFlat.js
+++ Impelmenting Arr Methods/15.MyFlat.js
@@ -1,4 +1,4 @@
-const array = [1, 2, [3, 4, [5, [6]], 7]];
+const array = [1, 2, [3, 4, [5, [6]], 7], 8];
 const myFlat = (arr, n) => {
   if (n === 0) {
     return arr;
@@ -7,12 +7,33 @@ const myFlat = (arr, n) => {
   const newArr = [];
   for (let x = 0; x < arr.length; x++) {
     if (Array.isArray(arr[x])) {
-      newArr.push(...arr[x]);
+      newArr.push(...myFlat(arr[x], --n));
     } else {
       newArr.push(arr[x]);
     }
   }
 
-  return myFlat(newArr, n - 1);
+  return newArr;
 };
-const res = myFlat(array, 1);
+const res = myFlat(array, 2);
+console.log(res);
+
+//version 2
+// const array = [1, 2, [3, 4, [5, [6]], 7]];
+// const myFlat = (arr, n) => {
+//   if (n === 0) {
+//     return arr;
+//   }
+
+//   const newArr = [];
+//   for (let x = 0; x < arr.length; x++) {
+//     if (Array.isArray(arr[x])) {
+//       newArr.push(...arr[x]);
+//     } else {
+//       newArr.push(arr[x]);
+//     }
+//   }
+
+//   return myFlat(newArr, n - 1);
+// };
+// const res = myFlat(array, 1);
